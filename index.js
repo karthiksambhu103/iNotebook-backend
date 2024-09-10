14efa6cce060d291efab7727a26d417f88cf1ed4
kartik sambhu <137057453+karthiksambhu103@users.noreply.github.com>
2024-09-10 23:39:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+const express = require('express');
+const cors = require('cors');
 const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors') 
 
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+// CORS configuration
+const corsOptions = {
+  origin: 'https://krs20notebook.netlify.app', // Your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'] // Allowed headers
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 // Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
-
-
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+  console.log(`iNotebook backend listening at http://localhost:${port}`);
+});
